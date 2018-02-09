4c68fe016a0db5c151bad44e8aac2a41824a6af8
Edgar Barajas <edgarsbarajas@yahoo.com>
2018-02-08 16:33:58 -0800

    Style header containing a nav and logo with flexbox. Add media query for scaling the logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Header from './components/header'
+
+import './styles/app.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      ejfbwjbnf
+        <Header/>
       </div>
     );
   }
---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/header.css'
+
+const Header = () => {
+  return(
+    <header>
+      <nav>
+        CREATE EVENT
+      </nav>
+      <img src='logo.png' alt='logo' className='logo'/>
+    </header>
+  )
+}
+
+export default Header;
