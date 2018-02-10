88e0103e69b78e7d632ae35d258b21a5105a84a9
Edgar Barajas <edgarsbarajas@yahoo.com>
2018-02-09 22:43:24 -0800

    Add loading spinner

--- src/components/searchResults.js
+++ src/components/searchResults.js
@@ -3,15 +3,19 @@ import Event from './event'
 import '../styles/searchResults.css'
 
 const SearchResults = ({events}) => {
-    return(
-      <div className='searchResults'>
-        { events.map((event, index) => {
-            return(
-              <Event key={index} {...event}/>
-            )
-        })}
-      </div>
-    )
+  var Spinner = require('react-spinkit');
+
+    
+      return(
+        <div className='searchResults'>
+          { events.map((event, index) => {
+              return(
+                <Event key={index} {...event}/>
+              )
+          })}
+        </div>
+      )
+
 }
 
 export default SearchResults;
