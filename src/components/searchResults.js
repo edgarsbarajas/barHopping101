80c22776c656ada199ab5ba88e9628afdc7e5473
Edgar Barajas <edgarsbarajas@yahoo.com>
2018-02-09 22:44:42 -0800

    Cleanup

--- src/components/searchResults.js
+++ src/components/searchResults.js
@@ -3,19 +3,15 @@ import Event from './event'
 import '../styles/searchResults.css'
 
 const SearchResults = ({events}) => {
-  var Spinner = require('react-spinkit');
-
-    
-      return(
-        <div className='searchResults'>
-          { events.map((event, index) => {
-              return(
-                <Event key={index} {...event}/>
-              )
-          })}
-        </div>
-      )
-
+  return(
+    <div className='searchResults'>
+      { events.map((event, index) => {
+          return(
+            <Event key={index} {...event}/>
+          )
+      })}
+    </div>
+  )
 }
 
 export default SearchResults;
