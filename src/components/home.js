6136df19f72fde43e32913139717025cb25294f8
Edgar Barajas <edgarsbarajas@yahoo.com>
2018-02-15 23:40:41 -0800

    Able to get the users location

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import SearchBar from './searchBar'
 import SearchResults from './searchResults'
+import axios from 'axios'
 import '../styles/home.css'
 
 class Home extends React.Component {
@@ -9,11 +10,48 @@ class Home extends React.Component {
 
     this.state = {
       events: [],
-      loading: false
+      loading: false,
+      userLocation: {}
     }
 
     console.log("state:", this.state);
   }
+
+  componentDidMount(){
+  //   axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyDeIdD4dZmfqED4gSOb_f7fErq0PdvUozI`)
+  //   .then(response => {
+  //     var lat = response.data.results[0].geometry.location.lat
+  //     var lng = response.data.results[0].geometry.location.lng
+  //     var coords = {
+  //       lat: lat,
+  //       lng: lng
+  //     }
+  //     console.log("YOYOYO", coords)
+  //
+  //   })
+  //   .catch(error => {
+  //     console.log(error);
+  //   });
+  // }
+  console.log("mapppsssssss");
+  axios.post(`https://www.googleapis.com/geolocation/v1/geolocate?key=${ENV['GOOGLE_MAPS_KEY']}`)
+  .then(response => {
+    // var lat = response.data.results[0].geometry.location.lat
+    // var lng = response.data.results[0].geometry.location.lng
+    // var coords = {
+    //   lat: lat,
+    //   lng: lng
+    // }
+    console.log("google maps");
+    console.log("YOYOYO", response)
+    console.log(response.data.location);
+    this.setState({ userLocation: {lat: response.data.location.lat, long: response.data.location.lng}})
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
 
   render(){
     return(
